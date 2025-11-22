d4bfcd0622a98373c777f84dc7f0b8cb7f110607
Hari G <ghari81189@gmail.com>
2025-11-22 22:00:54 +0530

    feat: Add GitHub Pages deployment configuration and root path redirect.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { QuadrantTrainerComponent } from './quadrant-trainer/quadrant-trainer.component';
 
-export const routes: Routes = [{ path: 'quadrant-trainer', component: QuadrantTrainerComponent }];
+export const routes: Routes = [
+  { path: 'quadrant-trainer', component: QuadrantTrainerComponent },
+  { path: '', redirectTo: 'quadrant-trainer', pathMatch: 'full' },
+];
